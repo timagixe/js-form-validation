472738183eb61c7f4763864e091ef00aa8979408
Tima Gixe <timagixe@protonmail.com>
2020-03-27 17:39:43 +0200

    improved the input checks

--- root/js/script.js
+++ root/js/script.js
@@ -3,9 +3,8 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
-const inputFields = [username, email, password, password2];
 
-// Show error in case of wrong input
+// SHOW ERROR WHEN INPUT IS INCORRECT
 function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
@@ -14,15 +13,49 @@ function showError(input, message) {
   small.innerText = message;
 }
 
-// Show success in case of correct input
+// SHOW SUCCESS WHEN INPIT IS CORRECT
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
 }
 
-// Email validation
+// CHECK USERNAME VALIDITY
+// ^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$
+//  └─────┬────┘└───┬──┘└─────┬─────┘└─────┬─────┘ └───┬───┘
+//        │         │         │            │           no _ or . at the end
+//        │         │         │            │
+//        │         │         │            allowed characters
+//        │         │         │
+//        │         │         no __ or _. or ._ or .. inside
+//        │         │
+//        │         no _ or . at the beginning
+//        │
+//        username is 8-20 characters long
+function checkUsername(input, min, max) {
+  if (input.value === '') {
+    showError(input, `${getFieldName(input)} is required`);
+    return undefined;
+  }
+
+  const regExp = new RegExp(
+    `^(?=.{${min},${max}}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$`
+  );
+
+  regExp.test(input.value)
+    ? showSuccess(input)
+    : showError(input, `${getFieldName(input)} is not valid`);
+}
+
+// CHECK EMAIL VALIDITY
 function checkEmail(input) {
-  const re = /\S+@\S+\.\S+/;
+  if (input.value === '') {
+    showError(input, `${getFieldName(input)} is required`);
+    return undefined;
+  }
+
+  const re = /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+  input.value = input.value.trim();
 
   if (re.test(String(input.value).toLowerCase())) {
     showSuccess(input);
@@ -31,59 +64,54 @@ function checkEmail(input) {
   }
 }
 
-// Setting input to must have
-function checkRequired(inputArray) {
-  inputArray.forEach(input => {
-    if (input.value === '') {
-      showError(input, `${getFieldName(input)} is required`);
-    } else {
-      showSuccess(input);
-    }
-  });
-}
+// CHECK PASSWORD VALIDITY
+function checkPassword(input, min, max) {
+  if (input.value === '') {
+    showError(input, `${getFieldName(input)} is required`);
+    return undefined;
+  }
 
-// Check the min/max length of the input value
-function checkLength(input, min, max) {
-  if (input.value.length < min) {
-    showError(
-      input,
-      `${getFieldName(input)} must be at least ${min} characters`
-    );
-  } else if (input.value.length > max) {
-    showError(
-      input,
-      `${getFieldName(input)} must be at least less than ${max} characters`
-    );
-  } else {
+  const regExp = new RegExp(
+    `^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{${min},${max}}$`
+  );
+
+  console.log(regExp);
+
+  let passwordLengthIsValid =
+    input.value.length >= min && input.value.length <= max ? true : false;
+  let passwordCharactersAreValid = regExp.test(input.value) ? true : false;
+
+  if (passwordLengthIsValid && passwordCharactersAreValid) {
     showSuccess(input);
+  } else {
+    showError(input, `${getFieldName(input)} is not valid`);
   }
 }
 
-// Check passwords match
+// CHECK PASSWORDS MATCH
 function checkPasswordMatch(input) {
   const [password1, password2] = input;
 
-  if (password1.value !== password2.value) {
-    showError(password2, 'Passwords do not match');
-  } else {
-    checkLength(password2, 6, 30);
-  }
+  password2.value === ''
+    ? showError(password2, 'Password confirmation is required')
+    : password1.value === password2.value
+    ? showSuccess(password2)
+    : showError(password2, `Passwords do not match`);
 }
 
-// Get field name with uppercased first char
+// GET FIELD NAME WITH UPPERCASED FIRST CHARACTER
 function getFieldName(input) {
   const firstLetterUpperCased = input.id.charAt(0).toUpperCase();
   const restOfTheWord = input.id.slice(1);
   return firstLetterUpperCased + restOfTheWord;
 }
 
-// Event listeners
+// EVENT LISTENERS
 form.addEventListener('submit', event => {
   event.preventDefault();
 
-  checkRequired(inputFields);
-  checkLength(username, 3, 15);
+  checkUsername(username, 3, 15);
   checkEmail(email);
-  checkLength(password, 6, 30);
+  checkPassword(password, 6, 30);
   checkPasswordMatch([password, password2]);
 });
